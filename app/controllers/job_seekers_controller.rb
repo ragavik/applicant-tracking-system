1d45ff1af6b38f1e76621749247bcb967f64c1e4
Abhay Soni <asoni3@ncsu.edu>
2018-03-02 21:16:29 -0500

    Fixed the issue with deleting job seekers

--- app/controllers/job_seekers_controller.rb
+++ app/controllers/job_seekers_controller.rb
@@ -1,6 +1,6 @@
 class JobSeekersController < ApplicationController
   before_action :set_job_seeker, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :destroy]
   # GET /job_seekers
   # GET /job_seekers.json
   def index
